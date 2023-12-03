24e45ae65b205a5a35451937b85e29a65329b2b7
Kibnakamoto <kibnakanoto@protonmail.com>
2023-12-03 18:00:54 -0500

    added naming feature for checkboxes

--- Android/app/src/main/java/com/example/grocerlist/MainActivity.java
+++ Android/app/src/main/java/com/example/grocerlist/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.grocerlist;
 
+import android.view.LayoutInflater;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -29,14 +33,58 @@ public class MainActivity extends AppCompatActivity {
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                addCheckbox();
+                openEditTextDialog();
             }
         });
     }
 
-    private void addCheckbox() {
+    // enter text of checkbox
+    private void openEditTextDialog() {
+        // Use a custom dialog or AlertDialog to prompt the user for text input
+        // For simplicity, this example uses a custom layout with an EditText
+        View dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_edittext, null);
+        EditText editText = dialogView.findViewById(R.id.editTextDialog);
+
+        // Create an AlertDialog and set the custom view
+        androidx.appcompat.app.AlertDialog alertDialog = new androidx.appcompat.app.AlertDialog.Builder(this)
+                .setTitle("Enter Text")
+                .setView(dialogView)
+                .setPositiveButton("Add", (dialog, which) -> {
+                    // Retrieve the entered text from the EditText
+                    String newText = editText.getText().toString().trim();
+
+                    // Check if the entered text is not empty
+                    if (!TextUtils.isEmpty(newText)) {
+                        // Add a new checkbox with the entered text
+                        addCheckbox(newText);
+                    } else {
+                        // Show a toast message if the entered text is empty
+                        Toast.makeText(MainActivity.this, "Text cannot be empty", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .create();
+
+        // Show the dialog
+        alertDialog.show();
+    }
+
+    private void addCheckbox(String text) {
         CheckBox newCheckbox = new CheckBox(this);
-        newCheckbox.setText("New Checkbox");
+        newCheckbox.setText(text);
+
+        // Set an OnCheckedChangeListener to handle checkbox state changes
+        newCheckbox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                // Apply or remove strikethrough based on the checkbox state
+                if (isChecked) {
+                    buttonView.setPaintFlags(buttonView.getPaintFlags() | android.graphics.Paint.STRIKE_THRU_TEXT_FLAG);
+                } else {
+                    buttonView.setPaintFlags(buttonView.getPaintFlags() & ~android.graphics.Paint.STRIKE_THRU_TEXT_FLAG);
+                }
+            }
+        });
         checkboxContainer.addView(newCheckbox);
     }
 }
